33cfeb8db49a17bebadc41719dcbbddbdaf0a1a4
sohier <sohier.lotfy.els@gmail.com>
2023-10-11 11:19:42 +0400

    fixing test case

--- test_wiki.py
+++ test_wiki.py
@@ -1,32 +1,45 @@
 import unittest
-import wiki
-from unittest.mock import patch
+from wiki import fetch_webpage_content, convert_to_markdown
+
 
 class TestWiki(unittest.TestCase):
-
-    @patch("requests.get")
-    def test_fetch_webpage_content(self, mock_requests_get):
-        mock_response = mock_requests_get.return_value
-        mock_response.text = "<html><body><div id='page'>Mocked Content</div></body></html>"
-
-        # Call the function with a mock URL
-        content_element = wiki.fetch_webpage_content("https://wiki.debian.org/News")
-
-        # Check if the function correctly returns the content element
+    def test_fetch_webpage_content(self):
+        wiki_url = "https://wiki.debian.org/News"
+        content_element = fetch_webpage_content(wiki_url)
         self.assertIsNotNone(content_element)
-        self.assertEqual(content_element.text.strip(), "Mocked Content")
+        self.assertIn('Debian has several news feeds', content_element)
 
     def test_convert_to_markdown(self):
-        mock_html = "<div id='page'>Mocked HTML Content</div>"
-
-        # Call the function to convert to Markdown
-        wiki.convert_to_markdown(mock_html, "test_output")
-
-        # Check if the Markdown file was created and has content
-        with open("test_output.md", "r", encoding="utf-8") as markdown_file:
+        sample_html = """
+        <h2 id="News_for_Debian_contributors">News for Debian contributors</h2>
+        <span class="anchor" id="line-23"></span>
+        <span class="anchor" id="line-24"></span>
+        <dl>
+            <dt>#debian-devel IRC channel topic </dt>
+            <dd>
+                <p class="line862">Current issues regarding Debian Project and Debian/<a
+                        href="https://wiki.debian.org/DebianUnstable">Unstable</a>. <small><br />
+                        <a class="ircs" href="ircs://irc.debian.org/#debian-devel">#debian-devel</a> topic is also available at
+                        <a href="https://wiki.debian.org/DevelopersCorner">DevelopersCorner</a>.</small> <span class="anchor"
+                        id="line-25"></span><span class="anchor" id="line-26"></span></p>
+            </dd>
+            <dt>debian-devel-announce </dt>
+            <dd>
+                <p class="line862">Announcements for <em>"developers"</em> (like policy changes, important release issues, teams
+                    news). <small><br />
+                        <a class="interwiki" href="https://lists.debian.org/debian-devel-announce"
+                            title="DebianList">debian-devel-announce</a> (<a
+                            href="https://wiki.debian.org/DeveloperNews">contribute</a>).</small> <span class="anchor"
+                        id="line-27"></span><span class="anchor" id="line-28"></span></p>
+        """
+        file_name = "test_output"
+        convert_to_markdown(sample_html, file_name)
+        with open(f"{file_name}.md", "r", encoding="utf-8") as markdown_file:
             markdown_content = markdown_file.read()
-            self.assertTrue(markdown_content.startswith("#"))
-            self.assertIn("Mocked HTML Content", markdown_content)
+            self.assertIn("#debian-devel IRC channel topic", markdown_content)
+            self.assertIn(
+                "[Unstable](https://wiki.debian.org/DebianUnstable)", markdown_content)
 
-if __name__ == "__main__": 
+
+if __name__ == "__main__":
     unittest.main()
--- wiki.py
+++ wiki.py
@@ -3,10 +3,11 @@ from bs4 import BeautifulSoup
 from markdownify import markdownify
 
 HEADER = {
-    "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-    "Accept-Language":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-    "Accept-Encoding":"gzip, deflate, br"
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+    "Accept-Language": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "Accept-Encoding": "gzip, deflate, br"
 }
+
 
 def fetch_webpage_content(url):
     """
@@ -22,22 +23,22 @@ def fetch_webpage_content(url):
             modified_link = "https://wiki.debian.org" + href
             link["href"] = modified_link
     response = str(soup)
+    print(response)
     return response
+
 
 def convert_to_markdown(html_content, file_name):
     """
     Converts HTML content to markdown format and save it to a file.
     """
     markdown_content = markdownify(str(html_content))
-    
+
     with open(f"{file_name}.md", "w", encoding="utf-8") as markdown_file:
         markdown_file.write(markdown_content)
+
 
 if __name__ == "__main__":
     wiki_page = "News"
     wiki_url = f"https://wiki.debian.org/{wiki_page}"
     content_element = fetch_webpage_content(wiki_url)
-    
-
     convert_to_markdown(content_element, wiki_page)
-
